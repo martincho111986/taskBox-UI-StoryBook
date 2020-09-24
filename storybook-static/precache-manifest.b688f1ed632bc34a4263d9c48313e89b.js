23a0387c6c4c4523bb7bfcd162f71be263400161
martin jerez <martincho111986@gmail.com>
2020-09-23 21:14:30 -0300

    taskbox UI

---
+++ storybook-static/precache-manifest.b688f1ed632bc34a4263d9c48313e89b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a8680e94800cb5fb321302519570a18",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.8547a732935a6dc0993b.bundle.js"
+  },
+  {
+    "url": "runtime~main.8547a732935a6dc0993b.bundle.js"
+  },
+  {
+    "revision": "0271c02c5e3facf9b478",
+    "url": "static/css/main.53f200ef.chunk.css"
+  },
+  {
+    "url": "vendors~main.8547a732935a6dc0993b.bundle.js"
+  },
+  {
+    "url": "vendors~main.8547a732935a6dc0993b.bundle.js.LICENSE.txt"
+  }
+]);
